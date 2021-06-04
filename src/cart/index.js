04fbfa46832ae447a13bb25987439c6ecdfe24a1
Tom-Lennart Sætran <79971807+tom-saetran@users.noreply.github.com>
2021-06-04 15:36:51 +0200

    Update index.js

--- src/cart/index.js
+++ src/cart/index.js
@@ -52,7 +52,7 @@ cartRouter.post("/remove", async (req, res, next) => {
 
 cartRouter.get("/", async (req, res, next) => {
     try {
-        const shoppingcart = await shopModel.find().populate("product_id", "reviews")
+        const shoppingcart = await shopModel.find().populate("product_id", !"reviews")
         res.send(shoppingcart)
     } catch (error) {
         console.log(error)
