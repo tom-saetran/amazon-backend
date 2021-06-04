f27d6f91bb8997a460e8160e12904953ec37e4d4
Tom-Lennart Sætran <79971807+tom-saetran@users.noreply.github.com>
2021-06-04 15:43:39 +0200

    Update schema.js

--- src/cart/schema.js
+++ src/cart/schema.js
@@ -5,6 +5,6 @@ const { Schema, model } = mongoose
 const shopSchema = new Schema({
     product_id: { type: Schema.Types.ObjectId, ref: "Product", required: true },
     quantity: { type: Number, default: 1 }
-})
+}, { timestamps: true })
 
 export default model("Cart", shopSchema)
