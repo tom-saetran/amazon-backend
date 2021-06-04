d8be826543c951182e99d0f367a3272b44211677
Tom-Lennart Sætran <79971807+tom-saetran@users.noreply.github.com>
2021-06-04 15:20:55 +0200

    Update schema.js

--- src/cart/schema.js
+++ src/cart/schema.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const { Schema, model } = mongoose
 
 const shopSchema = new Schema({
-    product_id: { type: Schema.Types.ObjectId, ref: "Product" },
+    product_id: { type: Schema.Types.ObjectId, ref: "Product", required: true },
     quantity: { type: Number, default: 1 }
 })
 
